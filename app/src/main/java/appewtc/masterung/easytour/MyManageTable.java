c46c68d86d48710501093a7d835b6c9765d5f134
masterUNG <phrombutr@gmail.com>
2016-02-01 19:29:53 +0700

    Finish 1Feb16

--- app/src/main/java/appewtc/masterung/easytour/MyManageTable.java
+++ app/src/main/java/appewtc/masterung/easytour/MyManageTable.java
@@ -12,6 +12,13 @@ public class MyManageTable {
     private MyOpenHelper objMyOpenHelper;
     private SQLiteDatabase writeSqLiteDatabase, readSqLiteDatabase;
 
+    public static final String table_user = "userTABLE";
+    public static final String column_id = "_id";
+    public static final String column_user = "User";
+    public static final String column_password = "Password";
+    public static final String column_name = "Name";
+    public static final String column_status = "Status";
+
     public MyManageTable(Context context) {
 
         //Create & Connected
@@ -20,5 +27,16 @@ public class MyManageTable {
         readSqLiteDatabase = objMyOpenHelper.getReadableDatabase();
 
     }   // Constructor
+
+    public long addUser(String strUser,
+                        String strPassword,
+                        String strName,
+                        String strStatus) {
+
+
+
+        return 0;
+    }
+
 
 }   // Main Class
