5009bc58fd47bbce42cbe6145a59b992ddf05e4a
masterUNG <phrombutr@gmail.com>
2016-02-01 19:15:24 +0700

    Create SQLite finish

--- app/src/main/java/appewtc/masterung/easytour/MainActivity.java
+++ app/src/main/java/appewtc/masterung/easytour/MainActivity.java
@@ -5,9 +5,18 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Explicit
+    private MyManageTable objMyManageTable;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Request database
+        objMyManageTable = new MyManageTable(this);
+
+    }   // Main Method
+
+}   // Main Class
---
+++ app/src/main/java/appewtc/masterung/easytour/MyOpenHelper.java
@@ -0,0 +1,35 @@
+package appewtc.masterung.easytour;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by masterUNG on 2/1/16 AD.
+ */
+public class MyOpenHelper extends SQLiteOpenHelper{
+
+    //Explicit
+    public static final String database_name = "easyTour.db";
+    private static final int database_version = 1;
+    private static final String create_user_table = "create table userTABLE (" +
+            "_id integer primary key, " +
+            "User text, " +
+            "Password text, " +
+            "Name text, " +
+            "Status text);";
+
+    public MyOpenHelper(Context context) {
+        super(context, database_name, null, database_version);
+    }   // Constructor
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(create_user_table);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}   // Main Class
