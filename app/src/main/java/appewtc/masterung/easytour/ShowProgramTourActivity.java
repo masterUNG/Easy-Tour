0a880f36cf9c60c6cbb32ba9922ab7ce554d0a9d
masterUNG <phrombutr@gmail.com>
2016-03-12 17:08:55 +0700

    Show Category finish

--- app/src/main/java/appewtc/masterung/easytour/ShowProgramTourActivity.java
+++ app/src/main/java/appewtc/masterung/easytour/ShowProgramTourActivity.java
@@ -3,23 +3,46 @@ package appewtc.masterung.easytour;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class ShowProgramTourActivity extends AppCompatActivity {
 
     //Explicit
     private double userLatADouble, userLngADouble;
     private String categoryString;
+    private TextView showCatTextView;
+    private ListView tourListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_program_tour);
 
+        //Bind Widget
+        bindWidget();
+
         //Receive Lat,Lng and Separate Group
         receiveAndSep();
 
+        //ShowView
+        showView();
+
 
     }   // Main Method
+
+    private void bindWidget() {
+        showCatTextView = (TextView) findViewById(R.id.textView11);
+        tourListView = (ListView) findViewById(R.id.listView);
+    }
+
+    private void showView() {
+
+        showCatTextView.setText(getResources().getString(R.string.listtour) + " " + categoryString);
+
+
+
+    }   // showView
 
     private void receiveAndSep() {
 
